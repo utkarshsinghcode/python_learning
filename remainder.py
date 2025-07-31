6952244df85d92e034f120143fcba1e7be08cd6d
utkarshsinghcode <mewadautkarsh341@gmail.com>
2025-07-31 17:26:41 +0530

    Create remainder.py

---
+++ remainder.py
@@ -0,0 +1,14 @@
+# Problem = pens dividing in student
+
+pen_nums = int(14)
+students_nums = 3   # have to divide equally
+
+# the nums of pen each student will get.
+
+result = int(pen_nums/3) 
+print("Pens for each student:",result)
+
+# nums of pens left
+
+left = pen_nums%3
+print("Remaining pens:",left)
