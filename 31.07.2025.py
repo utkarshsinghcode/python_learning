8605ac7251df70ae89cec243ea3068553e4192b0
utkarshsinghcode <mewadautkarsh341@gmail.com>
2025-08-01 15:00:14 +0530

    Update 31.07.2025.py

--- 31.07.2025.py
+++ 31.07.2025.py
@@ -29,3 +29,19 @@ volume_of_cube = length**3   #volume of cube is a**3.
 
 print(volume_of_cube)
 
+   # NEW CODE #
+# Problem = pens dividing in student
+
+pen_nums = int(14)
+students_nums = 3   # have to divide equally
+
+# the nums of pen each student will get.
+
+result = int(pen_nums/3) 
+print("Pens for each student:",result)
+
+# nums of pens left
+
+left = pen_nums%3
+print("Remaining pens:",left)
+
