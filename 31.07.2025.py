33f6ca51621541c4f38a9a4a1b7671d19a73f9a1
utkarshsinghcode <mewadautkarsh341@gmail.com>
2025-08-01 14:55:09 +0530

    Update 31.07.2025.py

--- 31.07.2025.py
+++ 31.07.2025.py
@@ -2,3 +2,13 @@ python_learning
 this is my python learning journe
 # newcode
 print("Hello world")
+
+# new code
+#division of two numbers
+
+num1 = 25
+num2 = 4
+
+divide_result = 25/4    # (/ divide always provide floating value)
+
+print(divide_result)
