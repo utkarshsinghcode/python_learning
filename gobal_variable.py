dc6650f043286f21d7af1e65bb9056d3c8e96139
utkarshsinghcode <mewadautkarsh341@gmail.com>
2025-08-01 12:21:33 +0530

    Create gobal_variable.py

---
+++ gobal_variable.py
@@ -0,0 +1,14 @@
+x = "awesome"
+y = "good"   #yeh ekk global varible hai
+
+
+def myfunc():#yrh def func hai
+     x = "cool"  #local variable
+     print("python is "+ x)
+    
+myfunc()   
+
+print("python is",x)
+    # globalvar. ko bahar and andar use kr sakte hai
+     
+     
