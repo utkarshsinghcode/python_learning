d1c7bba546fc1119b02e32c7924e3e6408f93d58
utkarshsinghcode <mewadautkarsh341@gmail.com>
2025-08-01 14:58:22 +0530

    Update 31.07.2025.py

--- 31.07.2025.py
+++ 31.07.2025.py
@@ -21,3 +21,11 @@ solve = 10/5*6
 print(solve)    #(python first goes with divide give 2.0 .
                 #( and then goes for multiply 2.0*6=12.0)
 
+        #NEW CODE#
+# volume of cube
+
+length = 5
+volume_of_cube = length**3   #volume of cube is a**3.
+
+print(volume_of_cube)
+
