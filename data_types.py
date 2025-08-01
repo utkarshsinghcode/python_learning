122b74789aed247d676ca46c9221ffb2d4bafed4
utkarshsinghcode <mewadautkarsh341@gmail.com>
2025-08-01 13:25:51 +0530

    Create data_types.py

---
+++ data_types.py
@@ -0,0 +1,37 @@
+x = "string hu bhai"
+y = 208
+z = 20.8
+l = ["apple","mango","banana"]
+t = ("grapes","cherry")
+d = {"name": "krishn", "age": "18"}
+b = True
+n = None
+print(x)
+print(type(x))
+
+print(y)
+print(type(y))
+
+print(z)
+print(type(z))
+
+print(l)
+print(type(l))
+
+print(t)
+print(type(t))
+
+print(d)
+print(type(d))
+
+print(b)
+print(type(b))
+
+print(n)
+print(type(n))
+
+
+
+
+
+
