7d26e67a08a820b863b1f345e0388c59fc001381
utkarshsinghcode <mewadautkarsh341@gmail.com>
2025-08-01 13:54:46 +0530

    Create convert_ntypes..py

---
+++ convert_ntypes..py
@@ -0,0 +1,26 @@
+# converting numberic types
+
+a = 20
+b = 25.5
+c = 2j
+
+#convert from int to float 
+
+x = float(a)
+
+#convert fron float to complex
+
+y = complex(b)
+
+#convert float to int 
+
+z = int(b)
+
+print(x)
+print(type(x))
+
+print(y)
+print(type(y))
+
+print(z)
+print(type(z))
